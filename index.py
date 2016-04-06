e380884e12bc96b255174c73f060d80401638157
flywen <flywen@flywen.cn>
2016-04-06 15:13:25 +0800

    Changes to be committed: 	modified:   index.py 	new file:   requirements.txt

--- index.py
+++ index.py
@@ -1,11 +1,13 @@
 #-*- coding:utf-8 -*-
+import django
 
 def app(environ, start_response):
     status = '200 OK'
     headers = [('Content-type', 'text/html')]
     start_response(status, headers)
     body=["Welcome to Flywen's blog!\n"]
-    return body
+    #return body
+    return 'django is:' + django.get_version()
 
 from bae.core.wsgi import WSGIApplication
 application = WSGIApplication(app)
