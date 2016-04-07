728d6adf0529a076930fde16266f256d9751730d
flywen@flywen.cn <flywen@flywen.cn>
2016-04-07 10:00:10 +0800

    add wenxin

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,52 @@
 from django.shortcuts import render
 from django.template.context_processors import request
 from django.http.response import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import hashlib
+from xml.etree import ElementTree as etree
+from django.utils.encoding import smart_str, smart_unicode
 
 # Create your views here.
 
 def test(request):
     return HttpResponse('it is a test for bae')
     
+# def weixin(request):
+#     return HttpResponse('it is a weixin!!! but you visit here xxx is an error')
+
+@csrf_exempt
 def weixin(request):
-    return HttpResponse('it is a weixin!!! but you visit here xxx is an error')
+    if request.method == "GET":
+        signature = request.GET.get("signature", None)
+        timestamp = request.GET.get("timestamp", None)
+        nonce = request.GET.get("nonce", None)
+        echostr = request.GET.get("echostr", None)
+        token = 'flywen'
+        tmp_list = [token, timestamp, nonce]
+        tmp_list.sort()
+        tmp_str = "%s%s%s" % tuple(tmp_list)
+        tmp_str = hashlib.sha1(tmp_str).hexdigest()
+        if tmp_str == signature:
+            return HttpResponse(echostr)
+        else:
+            return HttpResponse("这是微信接口，不提供其他服务！")
+
+    else:
+
+        xmlstr = smart_str(request.body)
+        xml = etree.fromstring(xmlstr)
+
+        ToUserName = xml.find('ToUserName').text
+        FromUserName = xml.find('FromUserName').text
+        CreateTime = xml.find('CreateTime').text
+        MsgType = xml.find('MsgType').text
+        Content = xml.find('Content').text
+        MsgId = xml.find('MsgId').text
+        reply_xml = """<xml>
+        <ToUserName><![CDATA[%s]]></ToUserName>
+        <FromUserName><![CDATA[%s]]></FromUserName>
+        <CreateTime>%s</CreateTime>
+        <MsgType><![CDATA[text]]></MsgType>
+        <Content><![CDATA[%s]]></Content>
+        </xml>"""%(FromUserName,ToUserName,CreateTime,Content + " 您好，公众号开发中，敬请期待...")
+        return HttpResponse(reply_xml,content_type='application/xml')
