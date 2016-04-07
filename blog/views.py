89441827964ec2d840abb5811450176d3f3c647b
flywen@flywen.cn <flywen@flywen.cn>
2016-04-07 13:19:36 +0800

    weather test

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,10 @@ from django.views.decorators.csrf import csrf_exempt
 import hashlib
 from xml.etree import ElementTree as etree
 from django.utils.encoding import smart_str, smart_unicode
+from jinja2._stringdefs import content
+import urllib2
+import json
+from nntplib import resp
 
 # Create your views here.
 
@@ -17,6 +21,7 @@ def test(request):
 
 @csrf_exempt
 def weixin(request):
+#     微信服务器使用GET方法发送验证信息
     if request.method == "GET":
         signature = request.GET.get("signature", None)
         timestamp = request.GET.get("timestamp", None)
@@ -49,5 +54,15 @@ def weixin(request):
         <CreateTime>%s</CreateTime>
         <MsgType><![CDATA[text]]></MsgType>
         <Content><![CDATA[%s]]></Content>
-        </xml>"""%(FromUserName,ToUserName,CreateTime,Content + " 还在开发中...")
+        </xml>"""%(FromUserName,ToUserName,CreateTime,getweather(Content) + " 还在开发中...")
         return HttpResponse(reply_xml,content_type='application/xml')
+    
+   
+def getweather(content):
+        url_start = 'http://op.juhe.cn/onebox/weather/query?cityname='
+        url_end = '&dtype=&key=2d887e93ed2cadde67d2a1f7d0d282c6'
+        url = url_start + content +url_end
+        resp = urllib2.urlopen(url)
+        weather = json.load(resp.read())
+        info = weather['result']['data']['life']['info']['kongtiao']
+        return info
